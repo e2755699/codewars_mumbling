3b55b0bd109536bb235cd67d8e32ebf42ca32302
e2755699 <e2755699@gmail.com>
2017-07-25 21:51:22 +0800

    refactor-use String.join

---
+++ codewars_mumbling/Mumbling.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace codewars_mumbling
+{
+    internal class Mumbling
+    {
+        internal string Accum(string input)
+        {
+
+            if (string.IsNullOrEmpty(input))
+            {
+                return string.Empty;
+            }
+
+            var output = new List<string>();
+            var count = 0;
+
+            foreach (char aChar in input)
+            {
+                string upperChar = aChar.ToString().ToUpper();
+                var subOutput = upperChar;
+                
+                for (int i = 0; i < count; i++)
+                {
+                    string lowerChar = upperChar.ToLower();
+                    subOutput = subOutput + lowerChar;
+                }
+
+                count++;
+
+                output.Add(subOutput);
+            }
+
+            return String.Join("-", output);
+
+        }
+    }
+}
